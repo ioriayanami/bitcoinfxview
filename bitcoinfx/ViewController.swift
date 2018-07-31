c9f033491bbd6ce658a38d703e589b36761d33b8
Iori Ayanami <iori@macbook-pro-2.local>
2018-07-31 14:04:23 +0900

    Initial Commit

---
+++ bitcoinfx/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  bitcoinfx
+//
+//  Created by Iori Ayanami on 2018/07/31.
+//  Copyright © 2018 iori. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
